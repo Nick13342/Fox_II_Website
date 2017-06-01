031327a5b3cfa22fa3d1422fdab091102567341b
Nick13342 <13342@burnside.school.nz>
2017-06-01 14:30:32 +1200

    Changed html txt so refers to Fox II Site

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-#cd 2017\Digital_Programming
-#c:\Python34\python.exe hello.py
+#cd 2017\DTP\Fox_II_Website
+#c:\Python34\python.exe app.py
 
 from flask import Flask, render_template
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
