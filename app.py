12b0a65e6302ec55b0cda5eed1105061489ce49d
Nick <13342@burnside.school.nz>
2017-06-17 12:34:38 +1200

    Updated Python

--- app.py
+++ app.py
@@ -8,5 +8,25 @@ app = Flask(__name__, template_folder="templates")
 def index():
     return render_template("index.html")
 
+@app.route("/rates/")
+def rates():
+    return render_template("rates.html")
+
+@app.route("/charter/")
+def charter():
+    return render_template("charter.html")
+
+@app.route("/about_us/")
+def about_us():
+    return render_template("about_us.html")
+
+@app.route("/faqs/")
+def faqs():
+    return render_template("faqs.html")
+
+@app.route("/not_available")
+def na():
+    return render_template("not_available.html")
+
 if __name__ == "__main__":
     app.run()
