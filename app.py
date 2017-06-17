376daec9cfab248c725bbdff121159376b60e6bb
Nick <13342@burnside.school.nz>
2017-06-17 15:14:25 +1200

    Added test tabel in database

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 #cd 2017\DTP\Fox_II_Website
 #c:\Python34\python.exe app.py
 
+import sqlite3
 from flask import Flask, render_template
 app = Flask(__name__, template_folder="templates")
 
+conn = sqlite3.connect('database/Fox_II.db')
+print ("Opened database successfully") #Confirm database connection.
+conn.close()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    con = sqlite3.connect("database/Fox_II.db")
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("select * from home")
+
+    rows = cur.fetchall();    
+    return render_template("index.html", rows = rows)
 
 @app.route("/rates/")
 def rates():
