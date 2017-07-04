d825c8fba2b0fda63aa0ab175805346e5878f5d8
Nick <13342@burnside.school.nz>
2017-07-04 13:56:10 +1200

    Added email class, updated customer.py

--- customer.py
+++ customer.py
@@ -1,148 +1,287 @@
 import sqlite3
+import datetime
+
+#------------------------------------------------------------------------------------
+# Class Name: Customer
+# Written By: Nick Glanville
+# Date:       02-07-2017
+#
+# Class to create allow an instance of an Customer object.  This class is designed to
+# perform all of the basic database functions on the Customer table.  It allows the
+# calling program to call simple methods when maintaining customers.  
+#------------------------------------------------------------------------------------
 
 class Customer:
     
+    #---------------------------------------------------------------------------------
+    # Initialse a blank customer structure
+    #---------------------------------------------------------------------------------
     def __init__(self):
-        self.__surname = ""
-        self.__firstname = ""
-        self.__email = ""
-        self.__dob = ""
-        self.__gender = ""
-        self.__phone = ""
-        self.__countryCode = ""
-        self.__lastBooking = ""
-        self.__totalBookings = ""
         
-    def readCust(self, con, CustID):
-        read_query = "select * from customer where CustID = ?"
-        
-        con.row_factory = sqlite3.Row
-        
-        cur = con.cursor()
-        cur.execute(read_query, (CustID,))
-        
-        row = cur.fetchone();
-        
-        self.__email = row['Email']
-        self.__surname = row['surname']
-        self.__firstname = row['firstname']
-        self.__dob = row['D.O.B']
-        self.__gender = row['gender']
-        self.__phone = row['phone']
-        self.__countryCode = row['CountryCode']
-        self.__lastBooking = row['lastBooking']
-        self.__totalBookings = row['totalBookings']        
+        # Create blank instance vriables for the new created object here.  We will
+        # prefix these with a single '_' to make clear that they are internal to this class.
+        self._CustID = 0
+        self._surname = ""
+        self._firstname = ""
+        self._email = ""
+        self._dob = None
+        self._gender = ""
+        self._phone = ""
+        self._countryCode = ""
+        self._lastBooking = None
+        self._totalBookings = 0
+ 
+    #---------------------------------------------------------------------------------
+    # 
+    #---------------------------------------------------------------------------------     
+    def __setCustomer(self,row):
+        # Allocate the retrieved columns into the object variables.
+        self._email = row['Email']
+        self._surname = row['surname']
+        self._firstname = row['firstname']
+        self._dob = row['DOB']
+        self._gender = row['gender']
+        self._phone = row['phone']
+        self._countryCode = row['CountryCode']
+        self._lastBooking = row['lastBooking']
+        self._totalBookings = row['totalBookings']
     
-    def updateCust(self, con, CustID):
-        self.__retvalue = True
-        #update_query = "update customer set Email = ?, surname = ?," \
-        #"firstname = ?, D.O.B = ?, gender = ?, phone = ?, CountryCode = ?," \
-        #"lastBooking = ?, totalBookings = ? where CustID = ?"
+    
+    
+    #-----------------------------------------------------------------------------------------------
+    # Read a customer record from the database.  Required is the database handle and the Customer ID
+    #-----------------------------------------------------------------------------------------------
+    def readCust(self, con, CustID):
+        # retValue contains the success or failure of the read operation. Default to success
+        self._retvalue = True
+        self._error = None
+        # define SQL query
+        read_query = "select * from customer where CustID = ?"
+       
+        try:
+            # define cursone and execute the query, CustID is the primary key so we will only expect
+            # one record to be returned.
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            cur.execute(read_query, (CustID,))
         
+            row = cur.fetchone();
+            
+            if row == None:
+                self._error = "No customer record found with CustID of: " + str(CustID)
+                self._retvalue = False
+            else:
+                self.__setCustomer(row)
+
+            
+        # Exception processing logic here.            
+        except Exception as err:
+            self._error = "Query Failed: " + str(err)
+            self._retvalue = False
+            
+        return self._retvalue
+  
+    #-----------------------------------------------------------------------------------------------
+    # Read a customer record from the database by email address.  Required is the database handle
+    # and email
+    #-----------------------------------------------------------------------------------------------
+    def readCustbyEmail(self, con, Email):
+        # retValue contains the success or failure of the read operation. Default to success
+        self._retvalue = True
+        self._error = None
+        # define SQL query
+        read_query = "select * from customer where CustID = ?"
+       
+        try:
+            # define cursone and execute the query, CustID is the primary key so we will only expect
+            # one record to be returned.
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            cur.execute(read_query, (CustID,))
+        
+            row = cur.fetchone();
+            
+            if row == None:
+                self._error = "No customer record found with CustID of: " + str(CustID)
+                self._retvalue = False
+            else:
+                self.__setCustomer(row)
+            
+        # Exception processing logic here.            
+        except Exception as err:
+            self._error = "Query Failed: " + str(err)
+            self._retvalue = False
+            
+        return self._retvalue
+    
+    #-------------------------------------------------------------------------------------------------
+    # 
+    # 
+    #-------------------------------------------------------------------------------------------------
+    def insertCust(self, con):
+        # retValue contains the success or failure of the update operation. Default to success
+        self._retvalue = True
+        self._error = None
+        
+        # define SQL query
         update_query = "update customer set Email = ?, surname = ?," \
-        "firstname = ?, D.O.B = ?, gender = ?, phone = ?, CountryCode = ? where CustID = ?"        
-        
-        #con.row_factory = sqlite3.Row
+        "firstname = ?, DOB = ?, gender = ?, phone = ?, CountryCode = ?," \
+        "lastBooking = ?, totalBookings = ? where CustID = ?"
+
+        # attempt to execute the query        
         try:
             cur = con.cursor()
-            cur.execute(update_query, (self.__email, self.__surname, self.__firstname, \
-                                       None, self.__gender, self.__phone, self.__countryCode, CustID))
         
-        #cur.execute(update_query, (self.__email, self.__surname, self.__firstname, \
-                                #self.__dob, self.__gender, self.__phone, self.__countryCode, \
-                                #self.__lastBooking, self.__totalBookings, CustID))        
+            cur.execute(update_query, (self._email, self._surname, self._firstname, \
+                                self._dob, self._gender, self._phone, self._countryCode, \
+                                self._lastBooking, self._totalBookings, CustID))        
         
+            # Commit the trasaction if successful.
             con.commit()
-        except:
-            self.retvalue = False
+            
+        # Exception processing logic here.    
+        except Exception as err:
+            self._error = "Query Failed: " + str(err)
+            # Rollback transaction if failed.
+            con.rollback()
+            self._retvalue = False
+                    
+        return self._retvalue
+  
+  
+    
+    #-------------------------------------------------------------------------------------------------
+    # Update a customer record from the database.  Required is the database handle and the Customer ID
+    # All fields will be updated with the object variables.
+    #-------------------------------------------------------------------------------------------------
+    def updateCust(self, con, CustID):
+        # retValue contains the success or failure of the update operation. Default to success
+        self._retvalue = True
+        self._error = None
         
-        return self.__retvalue
+        # define SQL query
+        update_query = "update customer set Email = ?, surname = ?," \
+        "firstname = ?, DOB = ?, gender = ?, phone = ?, CountryCode = ?," \
+        "lastBooking = ?, totalBookings = ? where CustID = ?"
+
+        # attempt to execute the query        
+        try:
+            cur = con.cursor()
+        
+            cur.execute(update_query, (self._email, self._surname, self._firstname, \
+                                self._dob, self._gender, self._phone, self._countryCode, \
+                                self._lastBooking, self._totalBookings, CustID))        
+        
+            # Commit the trasaction if successful.
+            con.commit()
+            
+        # Exception processing logic here.    
+        except Exception as err:
+            self._error = "Query Failed: " + str(err)
+            # Rollback transaction if failed.
+            con.rollback()
+            self._retvalue = False
+                    
+        return self._retvalue
+   
+    #------------------------------------------------------------------------------------------------
+    # If a new booking is made then we increment the total bookings and set the last booking
+    # date to be today
+    #------------------------------------------------------------------------------------------------
+    def newBooking(self):
+        self._totalBookings += 1
+        self._lastBooking = datetime.date.now().strftime ("%Y-%m-%d")
+   
+   
+    #-------------------------------------------------------------------------------------------------
+    # Expose the instance variables to calling programs using 'setter' and 'getter' routines instead
+    # of using individual methods.  This allows us to control how the properties are set and returned
+    # to the calling program.
+    # Do not create setter methods for totalBookings and lastBooking as they are controlled by the
+    # newBooking method
+    #------------------------------------------------------------------------------------------------- 
     
+    # ----- Customer ID ------
+    # Only allow a customer ID to be returned, do not allow setting as it's a system assigned field.
+    @property
+    def CustID(self):
+        return self._CustID
     
+    # ----- Email addreess ----
     @property
     def emailAddr(self):
-        return self.__email    
+        return self._email    
         
     @emailAddr.setter
     def emailAddr(self, emailAddr):
-        self.__email = emailAddr
+        self._email = emailAddr
     
-    
-    
+    # ----- Surname -----
     @property
     def surname(self):
-        return self.__surname  
+        return self._surname  
     
     @surname.setter
     def surname(self, surname):
-        self.__email = surname
-      
+        self._email = surname
     
-    
+    # ----- Firstname -----  
     @property
     def firstname(self):
-        return self.__firstname
+        return self._firstname
     
     @firstname.setter
     def firstname(self, firstname):
-        self.__email = firstname
-        
-        
+        self._email = firstname
     
+     # ----- Date of birth -----        
     @property
     def dob(self):
-        return self.__dob
+        return self._dob
     
     @dob.setter    
     def dob(self, dob):
-        self.__email = dob
-        
-        
-        
+        self._email = dob
+    
+    # ----- Gender -----    
     @property
     def gender(self):
-        return self.__gender
+        return self._gender
     
     @gender.setter    
     def gender(self, gender):
-        self.__email = gender
+        self._email = gender
         
-        
-        
+    # ----- Phone number ------        
     @property
     def phone(self):
-        return self.__phone
+        return self._phone
     
     @phone.setter    
     def phone(self, phone):
-        self.__email = phone
+        self._phone = phone
     
-    
-    
+    # ----- Country Code -----    
     @property
     def countryCode(self):
-        return self.__countryCode
+        return self._countryCode
     
     @countryCode.setter    
     def countryCode(self, countryCode):
-        self.__email = countryCode
-        
-        
-        
+        self._countryCode = countryCode
+    
+    # ----- lastBooking -----        
     @property
     def lastBooking(self):
-        return self.__lastBooking
+        return self._lastBooking
     
-    @lastBooking.setter    
-    def lastBooking(self, lastBooking):
-        self.__email = lastBooking
-        
-        
-        
+    # ----- total Bookings -----        
     @property
     def totalBookings(self):
-        return self.__totalBookings
-    
-    @totalBookings.setter    
-    def totalBookings(self, totalBookings):
-        self.__email = totalBookings
+        return self._totalBookings
+  
+    # ----- any error codes -----  
+    @property
+    def error(self):
+        return self._error 
+  
---
+++ emailAddress.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+#------------------------------------------------------------------------------------
+# Class Name: email
+# Written By: Nick Glanville
+# Date:       03-07-2017
+#
+# Class to create an instance of an email object.  At this stage the primary
+# function of the class is to validate the email address, but other functionality
+# related to an email can be added.
+#------------------------------------------------------------------------------------
+
+class email:
+    
+    #----------------------------------------------------------------------------------
+    # Maintain a list of common incorrect domain names here as class variables so they
+    # are consistant across all instances of the object
+    #----------------------------------------------------------------------------------
+    domains = []
+    domains.append("extra.co.nz")
+    domains.append("extra.com")
+    domains.append("xtra.com")
+    domains.append("gmail.co.nz")
+    domains.append("gmail.nz")
+    domains.append("google.co.nz")
+    
+  
+    #----------------------------------------------------------------------------------
+    # Initilise the object with email address and assume that it's a valid email
+    #----------------------------------------------------------------------------------
+    def __init__(self, NewEmail):
+        self._emailAddress = NewEmail
+  
+    #----------------------------------------------------------------------------------
+    # Method here to check the email address
+    # Several checks are made and the method returns immediately there is a failed
+    # check.  Other checks may be added to tigten up on invalid emails when required
+    #----------------------------------------------------------------------------------  
+    def validEmailAddress(self):
+        self._error = ""
+      
+        # Blank is not a valid email address  
+        if not self._emailAddress:
+            self._error = "Email address is blank"
+            return(False)
+        
+        # Email addresses must contain an @ symbol
+        if '@' not in self._emailAddress:
+            self._error = "Email address does not contain @"
+            return(False)
+        
+        # Cannot start with an @ or a . character
+        if self._emailAddress[0] == '@' or self._emailAddress[0] == '.':
+            self._error = "Email address cannot begin with @ or ."   
+            return(False)
+        
+        # Cannot finish with an @ or a . character     
+        if self._emailAddress[-1:] == '@' or self._emailAddress[-1:] == '.':
+            self._error = "Email address cannot end with @ or ."   
+            return(False)
+        
+        # Now lets split the email address on the @ symbol for more checking   
+        self._emailParts = self._emailAddress.split('@')
+        
+        # Cannot have multiple @ symbols 
+        if len(self._emailParts) > 2:
+           self._error = "Email address cannot have multiple @ characters"   
+           return(False)
+        
+        # cannot have a . either side of a @ system
+        if self._emailParts[0][-1:] == '.' or self._emailParts[1][0] == '.':
+            self._error = "Email address cannot have a . before or after the @"
+            return(False)
+
+        # Lets now check against some commom mistaken domain names
+        for self._domain in email.domains:
+            if self._emailParts[1].lower() == self._domain:
+               self._error = "Invalid domain name: " + self._emailParts[1].lower()
+               return(False)
+        
+        # if we have got this far we will call it a valid email address
+        return(True)    
+                
+  #---------------------------------------------------------------------------------
+  #  Define getter and setter routines here.
+  #---------------------------------------------------------------------------------
+  
+  
+  # Allow calling program to interrogate the error code
+    @property
+    def error(self):
+        return self._error
+    
+    @property
+    def email_count(self):
+        return email.count
